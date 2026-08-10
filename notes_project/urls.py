b013a8d06e2cd246ff4689f6f85fa4dfe6072fa6
Alexander Halim <igarashikaiyo@gmail.com>
2026-08-10 16:57:29 -0700

    adding redirect authenticated users from login page

--- notes_project/urls.py
+++ notes_project/urls.py
@@ -16,9 +16,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(
+        "accounts/login/",
+        auth_views.LoginView.as_view(redirect_authenticated_user=True),
+        name="login",
+    ),
     path("accounts/", include("django.contrib.auth.urls")),
     path("", include("notes.urls")),
 ]
